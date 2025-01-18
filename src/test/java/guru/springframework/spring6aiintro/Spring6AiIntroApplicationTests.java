e09b0c7ad9a49a9712ed1f83537a1e8c0975e430
dboeckli <dboeckli@gmail.com>
2025-01-18 18:48:52 +0100

    add test

--- src/test/java/guru/springframework/spring6aiintro/Spring6AiIntroApplicationTests.java
+++ src/test/java/guru/springframework/spring6aiintro/Spring6AiIntroApplicationTests.java
@@ -3,7 +3,7 @@ package guru.springframework.spring6aiintro;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@SpringBootTest(useMainMethod = SpringBootTest.UseMainMethod.ALWAYS)
 class Spring6AiIntroApplicationTests {
 
     @Test
