f458910ca9f956c4b86621a236a8599bd35f01f2
Dominique Boeckli <dboeckli@gmail.com>
2025-07-29 14:39:14 +0200

    fixup! set utility class

--- src/main/java/guru/springframework/spring6aiintro/service/AiResponseFormatter.java
+++ src/main/java/guru/springframework/spring6aiintro/service/AiResponseFormatter.java
@@ -4,12 +4,14 @@ import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.SerializationFeature;
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
+import lombok.experimental.UtilityClass;
 import org.springframework.ai.chat.model.ChatResponse;
 
 import java.util.LinkedHashMap;
 import java.util.Map;
 
-public class AiResponseFormatter {
+@UtilityClass
+public final class AiResponseFormatter {
 
     public static final String INPUT_CHECK_AI = "2+2? Short.";
 
